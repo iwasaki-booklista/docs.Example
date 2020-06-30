0274a6225b89e5c44667a9cceb808199da2e5348
iwasaki-booklista <iwasaki@booklista.co.jp>
2020-06-30 14:43:33 +0900

    add documents/

---
+++ docs/precache-manifest.815254ef6ec584c75140356fae0bcb6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8536662c4c60bc4daea31d3efe9ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79fcf726689d81488fd1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70d626f2ec6ebb4d8033",
+    "url": "/static/js/2.57dc4ec9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57dc4ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fcf726689d81488fd1",
+    "url": "/static/js/main.f7da1c92.chunk.js"
+  },
+  {
+    "revision": "6515c6f75477e4a967d7",
+    "url": "/static/js/runtime-main.3eaadc7d.js"
+  }
+]);
